114a2b7332ee896b8a8e09cbcdcd724034a6ac0e
Dominik Sumer <domi.sumer@gmail.com>
2025-06-02 15:49:43 +0200

    adjusted version for script

--- src/Frontend/ScriptLoader.php
+++ src/Frontend/ScriptLoader.php
@@ -31,7 +31,7 @@ class ScriptLoader {
             VMTRC_SCRIPT_HANDLE,
             $script_url,
             [],
-            '0.9.0', // script-version
+            '0.9.0', //scriptversion
             false
         );
 
